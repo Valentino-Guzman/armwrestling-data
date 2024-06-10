ded38f1bace93ed049e2b66efd7df482f05fc59d
scino11 <prod.scino@gmail.com>
2024-06-10 17:19:44 -0300

    boton about me

--- src/app/data-luchador/data-luchador.component.ts
+++ src/app/data-luchador/data-luchador.component.ts
@@ -11,8 +11,8 @@ import { MattComponent } from "./matt/matt.component";
   imports: [HeaderComponent, DevonComponent, MattComponent]
 })
 export class DataLuchadorComponent {
-  mostrarDatosDevon: string = 'he start for shes grandma';
-  mostrarDatosMatt: string = 'he start for shes grandmadsad';
+  mostrarDatosDevon: string = 'Devon Larratt (born 24 April 1975) is a Canadian professional armwrestler, content creator, and a former member of the Canadian Armed Forces.Considered to be one of the best arm wrestlers in the world and as of the King Of The Table 7 event, the title holder of the best arm wrestler in North America, he has collaborated to popularize the sport to a wider audience.';
+  mostrarDatosMatt: string = 'Mask is a 10-year veteran of the sport and is one of WALs most intense contenders. Mask has numerous WAL Regional Left and Right Hand Heavyweight titles to his name but is yet to take a Hammer at the WAL Championships, arguably it won’t be long until he does so. He has faced many of the sports legends, including John Brzenk and has proven he is one of the best in the business.';
   mensajeRecibidoDevon: string = '';
   mensajeRecibidoMatt: string = '';
   clicked: boolean = false;
